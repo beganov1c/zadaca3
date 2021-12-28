a40b2e9dd3a63fc4f1dd487ab90ca6a76a7bf192
beganov1c <tbeganovic1@etf.unsa.ba>
2021-12-28 16:14:09 +0100

    Dodan novi prozor.

---
+++ src/ba/unsa/etf/rpr/Novi.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+
+public class Novi {
+    public void cancelClick(ActionEvent actionEvent) {
+    }
+
+    public void okClick(ActionEvent actionEvent) {
+    }
+}
