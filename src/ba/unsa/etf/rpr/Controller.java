41373e87b93f8cfcfd3091f5379fb8d8b0427459
beganov1c <tbeganovic1@etf.unsa.ba>
2021-12-28 16:13:50 +0100

    Dodan glavni prozor.

---
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void numberClick(ActionEvent actionEvent) {
+
+
+    }
+
+    public void unosClick (ActionEvent actionEvent) {
+
+
+    }
+}
