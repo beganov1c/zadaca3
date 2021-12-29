2ea5da000a4a33f95d2be6a68e9ed7930be8de2e
beganov1c <tbeganovic1@etf.unsa.ba>
2021-12-29 04:08:42 +0100

    Dodana StudentiModel klasa.

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -1,24 +1,58 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class Controller implements Initializable {
+    public TextField fldText;
+    public Slider sliderStudents;
+    public ChoiceBox choiceColor;
+    public ListView lvStudents;
+    public GridPane gridPane;
+
+    private String color = "Default";
+
+    private StudentiModel studentiModel = new StudentiModel();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
-    public void numberClick(ActionEvent actionEvent) {
-
+    @FXML
+    public void initialize() {
+        lvStudents.setItems(StudentiModel.expandTo((int)sliderStudents.getValue(),fldText.getText()));
 
     }
 
-    public void unosClick (ActionEvent actionEvent) {
+    public void numberClick(ActionEvent actionEvent) {
 
+        Button broj = (Button) actionEvent.getSource();
+        fldText.appendText(broj.getText());
+
+    }
+
+    public void unosClick (ActionEvent actionEvent) throws IOException {
+
+        Stage novi = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/novi.fxml"));
+        loader.load();
+        novi.setTitle("Unos studenta");
+        novi.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        novi.setResizable(false);
+        novi.show();
 
     }
 }
---
+++ src/ba/unsa/etf/rpr/StudentiModel.java
@@ -0,0 +1,62 @@
+package ba.unsa.etf.rpr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class StudentiModel {
+
+    private static ObservableList<String> studenti = FXCollections.observableArrayList();
+    private static int trenutni=1;
+
+    public static void dodajStudenta(String broj) {
+        if(trenutni!=15) studenti.add("Student"+trenutni++);
+        else studenti.add("Student"+broj);
+    }
+
+    public static void dodajStudentaIme(String ime) {
+        studenti.add(ime);
+        trenutni++;
+    }
+
+
+    public static ObservableList<String> expandTo(int broj, String zadnji) {
+
+        if(broj<studenti.size()) return shrinkTo(broj);
+        for(int i=trenutni; i<broj; i++) {
+            if(studenti.size()==broj)
+                break;
+            dodajStudenta(zadnji);
+        }
+
+        return studenti;
+    }
+
+    public static ObservableList<String> shrinkTo(int broj) {
+        ObservableList<String> skraceni = FXCollections.observableArrayList();
+        int brojac=0;
+
+        for(String student : studenti) {
+            skraceni.add(student);
+            brojac++;
+            if(brojac==broj) break;
+        }
+
+        return skraceni;
+    }
+
+    public ObservableList<String> getStudenti() {
+        return studenti;
+    }
+
+    public void setStudenti(ObservableList<String> studenti) {
+        this.studenti = studenti;
+    }
+
+    public int getTrenutni() {
+        return trenutni;
+    }
+
+    public void setTrenutni(int trenutni) {
+        this.trenutni = trenutni;
+    }
+}
